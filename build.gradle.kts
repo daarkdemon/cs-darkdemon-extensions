d6777dd6596529bae17fba33601811787c44b6f2
darkdemon <dspatil11599@gmail.com>
2022-09-23 18:41:17 +0530

    Add Prmovies and OnlineMoviesHindi Sources

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ subprojects {
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
         // you can modify it to use other git hosting services, like gitlab
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/user/repo")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/daarkdemon/cs-darkdemon-extensions")
     }
 
     android {
