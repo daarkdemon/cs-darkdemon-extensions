21297f1ef585b931fa847d1b776a5b8c55deeb8a
darkdemon <dspatil11599@gmail.com>
2022-10-29 05:04:21 +0530

    add: NollyVerse Source

---
+++ NollyVerseProvider/build.gradle.kts
@@ -0,0 +1,26 @@
+version = 1
+
+
+cloudstream {
+    language = "en"
+    // All of these properties are optional, you can safely remove them
+
+    // description = "Lorem Ipsum"
+     authors = listOf("darkdemon")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+        "AsianDrama"
+    )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=nollyverse.com&sz=%size%"
+}
