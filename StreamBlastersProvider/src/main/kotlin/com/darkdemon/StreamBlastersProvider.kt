f345ecc130dd17d5507603f0a3f0ec8858531654
Bharathi <96393348+bachig26@users.noreply.github.com>
2022-12-09 21:45:05 +0100

    update StreamBlasters domain

--- StreamBlastersProvider/src/main/kotlin/com/darkdemon/StreamBlastersProvider.kt
+++ StreamBlastersProvider/src/main/kotlin/com/darkdemon/StreamBlastersProvider.kt
@@ -11,7 +11,7 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 
 class StreamBlastersProvider : MainAPI() { // all providers must be an instance of MainAPI
-    override var mainUrl = "https://streamblasters.pics"
+    override var mainUrl = "https://streamblasters.art"
     override var name = "StreamBlasters"
     override val hasMainPage = true
     override var lang = "hi"
