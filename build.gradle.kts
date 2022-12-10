785d0958f56b4e0aea66716611412566327e0146
Deepak Patil <dspatil11599@gmail.com>
2022-12-10 22:42:12 +0530

    fix(github pages): update build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,10 +76,11 @@ subprojects {
         // but you dont need to include any of them if you dont need them
         // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle
         implementation(kotlin("stdlib")) // adds standard kotlin features, like listOf, mapOf etc
-        implementation("com.github.Blatzar:NiceHttp:0.4.0") // http library
+        implementation("com.github.Blatzar:NiceHttp:0.4.1") // http library
         implementation("org.jsoup:jsoup:1.15.3") // html parser
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
         implementation("io.karn:khttp-android:0.1.2")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     }
 }
 
